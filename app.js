67422426a00e9b7a037c4c467a6ddde4c975d675
mzsigler <mzsigler@gmail.com>
2022-02-28 09:56:07 -0600

    Basic gameplay functional

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function getUserInput() {
+    let choices = ["rock", "paper", "scissors"]
     let userInput = prompt("Rock, Paper, or Scissors?");
     let choice = userInput.toLowerCase();
-    return choice;
+    
+    if (choices.includes(choice)) {
+        return choice
+    } else {
+        alert("That isn't a valid selection.")
+    }
 }
 
 function getRandomInt(max) {
@@ -20,3 +26,44 @@ function computerGuess() {
         return "scissors"
     }
 }
+
+function playRound() {
+    let userChoice = getUserInput();
+    let compChoice = computerGuess();
+
+    if (userChoice == "rock") {
+        if (compChoice == "rock") {
+            alert(`Tie! You both picked ${userChoice}`)
+        } if (compChoice == "scissors") {
+            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        } if (compChoice == "paper") { 
+            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
+            
+        }
+    }
+
+    if (userChoice == "paper") {
+        if (compChoice == "paper") {
+            alert(`Tie! You both picked ${userChoice}`)
+        } if (compChoice == "scissors") {
+            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        } if (compChoice == "rock") { 
+            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
+            
+        }
+    }
+
+    if (userChoice == "scissors") {
+        if (compChoice == "scissors") {
+            alert(`Tie! You both picked ${userChoice}`)
+        } if (compChoice == "paper") {
+            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        } if (compChoice == "rock") { 
+            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
+            
+        }
+    }
+
+    
+}
+
