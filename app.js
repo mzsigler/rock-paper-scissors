47b2a74970a548ea0ff4bc65fca0fc9d7ae6a7e2
mzsigler <mzsigler@gmail.com>
2022-03-26 12:17:21 -0500

    Rock selection now fully functional. Score updates and a winner is declared

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-function getUserInput() {
-    let choices = ["rock", "paper", "scissors"]
-    let userInput = prompt("Rock, Paper, or Scissors?");
-    let choice = userInput.toLowerCase();
-    
-    if (choices.includes(choice)) {
-        return choice
-    } else {
-        alert("That isn't a valid selection.")
-    }
+let playerScore = 0;
+let computerScore = 0; 
+
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorButton = document.getElementById('scissors')
+
+
+const playerScoreDisplay = document.getElementById("player-score");
+const computeScoreDisplay = document.getElementById("computer-score");
+const results = document.getElementById('results');
+
+
+playerScoreDisplay.innerText = `Player: ${playerScore}`;
+computeScoreDisplay.innerText = `Computer: ${computerScore}`;
+results.innerText = `Hey good job, you have ${playerScore} points and the computer has ${computerScore} points.`
+
+
+
+
+function scoreChecker() {
+    if (playerScore == 5) {
+        results.innerText = `You win! You made it to 5 points, the computer has ${computerScore}.`;
+     }
+     if (computerScore == 5) {
+        results.innerText = `You lost! The computer made it to 5 points and you have ${playerScore}.`;
+
+     }
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-function computerGuess() {
+function computerPicker() {
 
     let num = getRandomInt(3);
 
@@ -22,44 +40,56 @@ function computerGuess() {
         return "rock"
     } if (num == 1) {
         return "paper"
-    } else {
+    } if (num == 3) {
         return "scissors"
     }
 }
 
-function playRound() {
-    let userChoice = getUserInput();
-    let compChoice = computerGuess();
+function playRound(userChoice) {
+
+    let computerChoice = computerPicker();
+
 
     if (userChoice == "rock") {
-        if (compChoice == "rock") {
-            alert(`Tie! You both picked ${userChoice}`)
-        } if (compChoice == "scissors") {
-            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
-        } if (compChoice == "paper") { 
-            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        if (computerChoice == "rock") {
+            results.innerText = `Tie! You both picked ${userChoice}.`;
+        } if (computerChoice == "scissors") {
+            playerScore++;
+            playerScoreDisplay.innerText = `Player: ${playerScore}`;
+            results.innerText = `You win! You picked ${userChoice} and the computer picked ${computerChoice}!`
+            scoreChecker();
+
+        } if (computerChoice == "paper") { 
+            computerScore++;
+            computeScoreDisplay.innerText = `Computer ${computerScore}`;
+            results.innerText = `You lose! You picked ${userChoice} and the computer picked ${computerChoice}!`;
+            scoreChecker();
             
         }
     }
 
     if (userChoice == "paper") {
-        if (compChoice == "paper") {
+        if (computerChoice == "paper") {
             alert(`Tie! You both picked ${userChoice}`)
-        } if (compChoice == "scissors") {
-            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
-        } if (compChoice == "rock") { 
-            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        } if (computerChoice == "scissors") {
+            alert(`You lose! You picked ${userChoice} and the computer picked ${computerChoice}!`)
+            computerScore++;
+        } if (computerChoice == "rock") { 
+            alert(`You win! You picked ${userChoice} and the computer picked ${computerChoice}!`)
+            playerScore++;
             
         }
     }
 
     if (userChoice == "scissors") {
-        if (compChoice == "scissors") {
+        if (computerChoice == "scissors") {
             alert(`Tie! You both picked ${userChoice}`)
-        } if (compChoice == "paper") {
-            alert(`You win! You picked ${userChoice} and the computer picked ${compChoice}!`)
-        } if (compChoice == "rock") { 
-            alert(`You lose! You picked ${userChoice} and the computer picked ${compChoice}!`)
+        } if (computerChoice == "paper") {
+            playerScore++;
+            alert(`You win! You picked ${userChoice} and the computer picked ${computerChoice}!`)
+        } if (computerChoice == "rock") { 
+            computerScore++;
+            alert(`You lose! You picked ${userChoice} and the computer picked ${computerChoice}!`)
             
         }
     }
@@ -67,3 +97,8 @@ function playRound() {
     
 }
 
+let computerChoice = computerPicker();
+
+rockButton.addEventListener('click', function() {
+    playRound("rock")
+});
